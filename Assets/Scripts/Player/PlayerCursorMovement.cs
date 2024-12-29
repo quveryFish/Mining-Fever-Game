44db1acd178da3148472e411e5c53fb3a899dbbd
quveryFish <yaromdem@gmail.com>
2024-12-29 21:16:10 +0200

    Update player

--- Assets/Scripts/Player/PlayerCursorMovement.cs
+++ Assets/Scripts/Player/PlayerCursorMovement.cs
@@ -5,19 +5,23 @@ public class PlayerCursorMovement : MonoBehaviour
     [SerializeField] private int maxSpeed = 50;
     void Update()
     {
-        if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x < 9 &&
-            Camera.main.ScreenToWorldPoint(Input.mousePosition).x > -9 &&
-            Camera.main.ScreenToWorldPoint(Input.mousePosition).y < 4 &&
-            Camera.main.ScreenToWorldPoint(Input.mousePosition).y > -5)
+        Vector2 mousePos = Input.mousePosition;
+        if (Camera.main.ScreenToWorldPoint(mousePos).x < 9 &&
+            Camera.main.ScreenToWorldPoint(mousePos).x > -9 &&
+            Camera.main.ScreenToWorldPoint(mousePos).y < 4 &&
+            Camera.main.ScreenToWorldPoint(mousePos).y > -5)
         {
             transform.position = Vector2.MoveTowards(transform.position,
-            Camera.main.ScreenToWorldPoint(Input.mousePosition),
+            Camera.main.ScreenToWorldPoint(mousePos),
             maxSpeed * Time.deltaTime);
         }
         else
         {
             transform.position.Set(0, 0, 0);
         }
-
+        Vector2 direction = new Vector2(
+            Camera.main.ScreenToWorldPoint(mousePos).x,
+            Camera.main.ScreenToWorldPoint(mousePos).y);
+        transform.up = direction;
     }
 }
